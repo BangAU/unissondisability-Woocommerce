e78266988d2deaa3231a66aa686d8f2e3fe27f63
Sunil Singh <sunilsingh2019@gmail.com>
2022-05-17 09:36:04 +1000

    email template

--- wp-content/themes/storefrontchildtheme/inc/price-settings.php
+++ wp-content/themes/storefrontchildtheme/inc/price-settings.php
@@ -405,21 +405,52 @@
 // }
 
 
-function sku_before_order_item_name( $item_name, $item, $is_visible ) {     
-    $product = $item->get_product();
-    $sku = $product->get_sku();
+// function sku_before_order_item_name( $item_name, $item, $is_visible ) {     
+//     $product = $item->get_product();
+//     $sku = $product->get_sku();
 
-    // When sku doesn't exist we exit
-    if( empty( $sku ) ) return $item_name;
+//     // When sku doesn't exist we exit
+//     if( empty( $sku ) ) return $item_name;
 
-    $sku_text = __ . ': ' . $sku;
 
-    // Add product permalink when argument $is_visible is true
-    $product_permalink =  $is_visible ? $product->get_permalink( $item ) : '';
+//     // Add product permalink when argument $is_visible is true
+//     $product_permalink =  $is_visible ? $product->get_permalink( $item ) : '';
 
-    if( $product_permalink )
-        return sprintf( '<a href="%s">%s - %s</a>', $product_permalink, $sku_text, $item->get_name() );
-    else
-        return $item->get_name() . ' - ' . $sku_text;
+//     if( $product_permalink )
+//         return sprintf( '<a href="%s">%s - %s</a>', $product_permalink, $sku_text, $item->get_name() );
+//     else
+//         return $item->get_name() . ' - ' . $sku_text;
+// }
+// add_filter( 'woocommerce_order_item_name', 'sku_before_order_item_name', 30, 3 );
+
+/**
+ * Adds product images to the WooCommerce order emails table
+ * Uses WooCommerce 2.5 or newer
+ *
+ * @param string $output the buffered email order items content
+ * @param \WC_Order $order
+ * @return $output the updated output
+ */
+function sww_add_images_woocommerce_emails( $output, $order ) {
+	
+	// set a flag so we don't recursively call this filter
+	static $run = 0;
+  
+	// if we've already run this filter, bail out
+	if ( $run ) {
+		return $output;
+	}
+  
+	$args = array(
+        'show_sku'      => true,
+        'show_image'    => true,
+        'image_size'    => array( 100, 100 ),
+    );
+  
+	// increment our flag so we don't run again
+	$run++;
+  
+	// if first run, give WooComm our updated table
+	return $order->email_order_items_table( $args );
 }
-add_filter( 'woocommerce_order_item_name', 'sku_before_order_item_name', 30, 3 );
+add_filter( 'woocommerce_email_order_items_table', 'sww_add_images_woocommerce_emails', 10, 2 );
