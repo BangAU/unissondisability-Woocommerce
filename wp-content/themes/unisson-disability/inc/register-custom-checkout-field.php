816dc76362ca3afc31911325c14e45db58c81195
Sunil Singh <sunilsingh2019@gmail.com>
2022-06-07 10:00:24 +1000

    no message

--- wp-content/themes/unisson-disability/inc/register-custom-checkout-field.php
+++ wp-content/themes/unisson-disability/inc/register-custom-checkout-field.php
@@ -539,10 +539,10 @@ function my_custom_checkout_field_order_meta_keys( $keys ) {
         $x = 1;
         
         while ($x <= $quantity) {
-        echo '<div class="Attendee-group"><h4>'.  __('<span class="attendee-title">Attendee ' . $i . ' - </span>' )  . $_product->post_title .'</h4>';
-        $keys[] = 'First Name of Attendee'; 
-        $keys[] = 'Last Name of Attendee';
-        $keys[] = 'Funding Type of Attendee';
+        echo '<div class="Attendee-group"><h4>'.  __('<span class="attendee-title">Attendee ' . $x . ' - </span>' )  . $_product->post_title .'</h4>';
+        $keys[] = 'First Name of Attendee'.$x; 
+        $keys[] = 'Last Name of Attendee'.$x;
+        $keys[] = 'Funding Type of Attendee'.$x;
         echo '</div>';
         $x++;
     }
