f4fed9d090980b68262c3941b7e4ef39b3d7d8f6
Amar Shahukhal <amar.shahukhal@gmail.com>
2022-07-13 14:25:09 +1000

    temporarily removing conflicting code

--- wp-content/themes/unisson-disability/inc/module-restriction.php
+++ wp-content/themes/unisson-disability/inc/module-restriction.php
@@ -14,22 +14,21 @@ $allowed_block_types = array(
 );
 
 //adding modules to content pages
-if ($post->post_type === 'page') {
+// if ($post->post_type === 'page') {
 
-    array_push(
-        $allowed_block_types,
-        'acf/cta-module',
-        'acf/free-text-module',
-        'acf/hero-slider-module',
-        'acf/cta-module',
-        'acf/shop-module',
-        'acf/shop-module-ajax',
-        
-    );
+//     array_push(
+//         $allowed_block_types,
+//         'acf/cta-module',
+//         'acf/free-text-module',
+//         'acf/hero-slider-module',
+//         'acf/cta-module',
+//         'acf/shop-module',
+//         'acf/shop-module-ajax'
+//     );
 
-    return $allowed_block_types;
+//     return $allowed_block_types;
 
-}
+// }
 
 
 
