de984c0b1df688c814514fcb79adbd957bcad85c
Sunil Singh <sunilsingh2019@gmail.com>
2022-07-22 08:59:46 +1000

    a

--- wp-content/themes/unisson-disability/inc/register-custom-checkout-field_with_title.php
+++ wp-content/themes/unisson-disability/inc/register-custom-checkout-field_with_title.php
@@ -388,7 +388,7 @@ function person_details($checkout) {
         echo '</div>';
 //funding type starting
 
-                }}}
+ }}}
 
 /**
  * Save value of fields
@@ -536,7 +536,7 @@ function customise_checkout_field_update_order_meta($order_id) {
 
 
 
-// Display additional billing fields values
+//Display additional billing fields values
 // add_action('woocommerce_order_details_after_order_table', 'display_additional_billing_fields_values' ); // Order received and view
 // add_action( 'woocommerce_email_after_order_table', 'display_additional_billing_fields_values' ); // Email notifications
 // //add_action( 'woocommerce_admin_order_data_after_billing_address', 'display_additional_billing_fields_values' ); // Admin edit Order
@@ -576,4 +576,29 @@ function customise_checkout_field_update_order_meta($order_id) {
             
         
 //         echo '</table></div>';
-//}
+// }
+
+/**
+ * Add fields to order emails
+ **/
+add_action('woocommerce_order_details_after_order_table', 'my_custom_checkout_field_order_meta_keys' ); // Order received and view
+// add_action( 'woocommerce_email_after_order_table', 'my_custom_checkout_field_order_meta_keys' ); // Email notifications
+add_action( 'woocommerce_admin_order_data_after_billing_address', 'my_custom_checkout_field_order_meta_keys' ); // Admin edit Order
+add_action('woocommerce_email_order_meta_keys', 'my_custom_checkout_field_order_meta_keys');
+
+function my_custom_checkout_field_order_meta_keys( $keys ) {
+    global $woocommerce;
+    $count = $woocommerce->cart->cart_contents_count;
+    $i = 0;
+    echo '<h2>' . __( 'Attendee details', 'woocommerce' ) . '</h2>';
+           for($k=1; $k<= $count; $k++) {
+            $i++;
+        
+            $keys[] = 'First Name of Attendee'.$i; 
+            $keys[] = 'Last Name of Attendee'.$i;
+            
+        //echo '</div>';
+
+    }	
+    return $keys;
+    }
