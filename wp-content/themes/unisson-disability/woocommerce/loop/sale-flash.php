b14fe09a869f8846dfb5f28188012ae7bbe481ec
Sunil Singh <sunilsingh2019@gmail.com>
2022-07-22 13:30:40 +1000

    no message

--- wp-content/themes/unisson-disability/woocommerce/loop/sale-flash.php
+++ wp-content/themes/unisson-disability/woocommerce/loop/sale-flash.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Sidebar
+ * Product loop sale flash
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/global/sidebar.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/sale-flash.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -19,6 +19,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-get_sidebar( 'shop' );
+global $post, $product;
+
+?>
+<?php if ( $product->is_on_sale() ) : ?>
+
+	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . esc_html__( 'Sale!', 'woocommerce' ) . '</span>', $post, $product ); ?>
+
+	<?php
+endif;
 
 /* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
